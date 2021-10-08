d7f9573ac6af5629a9d24be25a712e9f1eb25480
Jay-R-H <jayhiremathr@gmail.com>
2021-10-08 16:15:36 +0530

    Added:Finding maximum integer value

--- src/com/programming/FindingMaximum.java
+++ src/com/programming/FindingMaximum.java
@@ -2,7 +2,17 @@ package com.programming;
 
 public class FindingMaximum {
 
+    public static Integer maxOfInteger(Integer a1, Integer a2, Integer a3) {
+        Integer max = a1;
+        if (a2.compareTo(max) > 0)
+            max = a2;
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
+        Integer a1 = 10, a2 = 8, a3 = 9;
+        System.out.println("Maximum value is : " + maxOfInteger(a1, a2, a3));
 
     }
-}
